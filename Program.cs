8ba74e492ad6b3d5043858d30ea18f7c6d64cdaa
Salvador <sarevalotamayo@student.rtc.edu>
2024-01-11 17:28:05 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GAStringsSalvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string myFirstName = "Salvador";
+            string myLastName = "Arevalo Tamayo";
+            string myFullName = myFirstName + " " + myLastName;
+            Console.WriteLine(myFullName);
+            
+
+        }
+    }
+}
